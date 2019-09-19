4c909a450c3ad1971734e05a12b37310d3b37211
Timothy Letkeman <tiggy02@gmail.com>
2019-09-19 14:16:19 -0600

    Got client ip.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let express = require('express');
+const express = require('express');
+
 let app = express();
 let serv = require('http').Server(app);
 
@@ -9,12 +10,26 @@ printLanAddress();
 
 let io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket) {
-  console.log('socket connection');
+  console.log('New connection: ID: ' + socket.id + " | IP: " + socket.request.connection.remoteAddress);
 
   socket.on('happy', function(data){
     console.log('happy message received: ' + data.reason);
+    resendPosition(socket, Math.random() * data.xSize, Math.random() * data.ySize);
   });
 });
+
+function resendPosition(socket, x, y)
+{
+  socket.emit('serverMsg', {
+    xPos: x,
+    yPos: y,
+  });
+}
+
+
+//SUGAR SUGAR SUGAR SUGAR SUGAR SUGAR SUGAR SUGAR SUGAR
+//SUGAR SUGAR SUGAR SUGAR SUGAR SUGAR SUGAR SUGAR SUGAR
+//SUGAR SUGAR SUGAR SUGAR SUGAR SUGAR SUGAR SUGAR SUGAR
 
 function printLanAddress() //https://stackoverflow.com/questions/3653065/get-local-ip-address-in-node-js
 {
--- client/sketch.js
+++ client/sketch.js
@@ -1,22 +1,32 @@
 let socket;
 
+let x = 0;
+let y = 0;
+
 function setup()
 {
     frameRate(60);
     createCanvas(windowWidth, windowHeight);
 
     socket = io();
+    
+    socket.on('serverMsg', function(data){
+        x = data.xPos;
+        y = data.yPos;
+    });
 }
 
 function draw()
 {
     background(190);
-    ellipse(50, 100, 20);
+    ellipse(x, y, 20);
 
     if (mouseIsPressed)
     {
-        socket.emit('happy',{
-            reason:'my socket server is running'
+        socket.emit('happy', {
+            reason:'my socket server is running',
+            xSize: windowWidth,
+            ySize: windowHeight
         });
     }
 }
